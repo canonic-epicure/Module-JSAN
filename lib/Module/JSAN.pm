a8a4a5003edfa8270458ad9d4994f50f7cacf4f0
SamuraiJack <nickolay8@gmail.com>
2009-10-13 19:42:47 +0400

    - Module::Build::JSAN added to bundling - some progress on README

--- lib/Module/JSAN.pm
+++ lib/Module/JSAN.pm
@@ -15,6 +15,7 @@ sub import {
         use inc::Module::Build::Functions(build_class => 'Module::Build::JSAN::Installable');
         
         Module::Build::Functions::copy_package('Module::JSAN');
+        Module::Build::Functions::copy_package('Module::Build::JSAN', 'true');
         Module::Build::Functions::copy_package('Module::Build::JSAN::Installable', 'true');
         
         Module::Build::Functions::_mb_required('0.35');
